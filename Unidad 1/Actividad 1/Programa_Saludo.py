9a38dc8b214a671bb2c00750605cdd40b06a0aaa
Santiago <L20100265@nlaredo.tecnm.mx>
2025-09-09 21:28:57 -0500

    Parte A - Programas Python

---
+++ Unidad 1/Actividad 1/Programa_Saludo.py
@@ -0,0 +1,9 @@
+#Parte A - Programas Python
+# a. Programa que muestre: "Hola, este es miprimer programa en python"
+print("Hola, este es mi primer programa en python.")
+
+# b. Programa que solicite nombre/edad y muestre: "Hola [nombre], tienes [edad] años"
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+print("Hola", nombre, "tienes", edad, "años")
