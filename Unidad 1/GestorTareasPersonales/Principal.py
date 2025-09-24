173946e9a00c365e5e153d8239dbe746a3e7cdbd
Santiago <L20100265@nlaredo.tecnm.mx>
2025-09-24 00:34:08 -0500

    fix: Correcion de nombres en funciones ya exustente feat: Creacion de funciones para las marcas de las tareas y su eliminacion

--- Unidad 1/GestorTareasPersonales/Principal.py
+++ Unidad 1/GestorTareasPersonales/Principal.py
@@ -1,6 +1,7 @@
 #lIbrerias utilizadas
 import json
 
+#Metodos definidos 
 def cargarTareas():
     #Verificacion de la existencia del archivo JSON
     try:
@@ -10,22 +11,22 @@ def cargarTareas():
         #Si el archivo no existe, se crea
         return []
     
-def guardarTareas(lista_de_tareas):
+def guardarTareas(listaDeTareas):
     #Abrimos el archivo y escribimos en el "w"
     with open('tareas.json', 'w') as archivo:
-        return json.dump(lista_de_tareas, archivo, indent=4)
+        return json.dump(listaDeTareas, archivo, indent=4)
     
-def agragarTareas(tareas):
+def agregarTareas(tareas):
     descripcion = input("Esriba la nueva tarea:")
 
     #Creamos un diccionario para la nueva tarea
-    nueva_tarea = {
+    nuevaTarea = {
         "descripcion": descripcion,
         "estado": "pendiente"
     }
 
     #Agregamos la tarea
-    tareas.append(nueva_tarea)
+    tareas.append(nuevaTarea)
     print("Tarea agregada con exito!")
     return tareas
 
@@ -36,5 +37,61 @@ def listarTareas(tareas):
         print("¡Sin tareas pendientes! Agrega una para empezar.")
         return
     
-    print
+    #Imprimimos el encabezado
+    print(f"{'No.':<5} | {'Descripicion':<30} | {'Estado'}")
+    print("-" * 50)
+
+    #Iteramos sobre la lista para imprimir cada tarea con su numero
+    for t, tareas in enumerate(tareas, start=1):
+        print(f"{t:<5} | {tareas['descripcion']:<30} | {tareas['estado']}")
+    
+    print("-" * 50)
    
+def marcarTarea(tareas):
+    #Mostramos las tareas disponibles a elegir
+    listarTareas(tareas)
+
+    #Sin tareas no se puede marcar tareas
+    if not tareas: 
+        return tareas
+    
+    try:
+        numTareas = int(input("Ingrese el numero de la tarea a completar"))
+
+        #Indice de la tarea a completar
+        indice = numTareas - 1
+
+        #Validamos que el indice este dentro del rango
+        if 0 <= indice < len(tareas):
+            tareas[indice]["estado"] = "completada"
+            print(f"Tarea '{tareas[indice]['descripcion']}' ha sido marcada como completada.")
+        else:
+            print("Numero de tarea no valido")
+
+    except ValueError:
+        print("Entrada no valida. Favor de ingresar un numero valido.")
+
+    return tareas
+
+def eliminarTarea(tareas):
+    listarTareas(tareas)
+
+    if not tareas:
+        return tareas
+    
+    try:
+        numTarea = int(input("Ingrese el numero de la tarea que desea eliminar: "))
+
+        indice = numTarea - 1
+
+        if 0 <= indice < len(tareas):
+            tareaEliminada = tareas[indice]['descripcion']
+            tareas.pop(indice)
+
+            print(f"Tarea '{tareaEliminada}' ha sido eliminada con exito. ")
+        else:
+            print("Numero de tarea no valido")
+    except ValueError:
+        print("Entrada no valida. Favor de ingresar otro numero.")
+
+    return tareas
