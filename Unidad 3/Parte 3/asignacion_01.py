72af9b7cdf792822894d104de7662027afde5531
Santiago <L20100265@nlaredo.tecnm.mx>
2025-12-03 23:56:24 -0600

    feat: Añadida fabrica de transformadores y reductores

--- Unidad 3/Parte 3/asignacion_01.py
+++ Unidad 3/Parte 3/asignacion_01.py
@@ -8,7 +8,25 @@ from functools import reduce
 def crearFiltro(predicado):
     """
     Recibe un predicado (función que devuelve bool) y retorna una 
-    NUEVA función que filtra una lista completa.
+    nueva función que filtra una lista completa.
     """
     #Retornamos una lambda que espera lista de datos
     return lambda lista: list(filter(predicado, lista))
+
+#Fábrica de Transformadores
+def crearTransformador(funcion):
+    """
+    Recibe una función de transformación y retorna una 
+    nueva función que aplica esa transformación a toda una lista.
+    """
+    # Retornamos una lambda que aplica map a la lista recibida
+    return lambda lista: list(map(funcion, lista))
+
+#Fábrica de Reductores
+def crearReductor(funcion, valorInicial):
+    """
+    Recibe una función de reducción y un valor inicial.
+    Retorna una nueva función que reduce una lista a un solo valor.
+    """
+    #Retornamos una lambda que aplica reduce a la lista recibida
+    return lambda lista: reduce(funcion, lista, valorInicial)
